458b6d26df0f24541922c615324d5f050a215d1f
Katri Vilonen <katri.vilonen@gmail.com>
2016-03-08 09:53:26 +0200

    sql changes and LoginController

---
+++ tyotunnit/src/main/java/fi/softala/yellow/servlet/LoginController.java
@@ -0,0 +1,39 @@
+package fi.softala.yellow.servlet;
+
+import java.io.IOException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@Controller
+@RequestMapping (value="/")
+public class LoginController {
+ 
+	@RequestMapping(value="/loginpage", method = RequestMethod.GET)
+	public String login(Model model) {
+ 
+		return "login";
+ 
+	}
+ 
+	@RequestMapping(value="/loginfail", method = RequestMethod.GET)
+	public String loginerror(Model model) {
+ 
+		model.addAttribute("loginerror", "true");
+		return "login";
+ 
+	}
+ 
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout(Model model) {
+
+		model.addAttribute("loggedout", "true");
+		return "login";
+ 
+	}
+	
+}
--- tyotunnit/tietokanta/create.sql
+++ tyotunnit/tietokanta/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Kayttajat (
-  id integer NOT NULL auto_increment PRIMARY KEY,
+  id SMALLINT NOT NULL auto_increment PRIMARY KEY,
   kayttajatunnus varchar(255) NOT NULL UNIQUE,
   password_encrypted varchar(255) NOT NULL,
   enabled tinyint NOT NULL,
@@ -8,26 +8,25 @@ CREATE TABLE Kayttajat (
   email varchar(50) default NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE Tunnit
-( tunti_id 			SMALLINT NOT NULL AUTO_INCREMENT
-, kayttaja_id 		SMALLINT NOT NULL
-, pvm 				DATE NOT NULL
-, tuntien_maara	 	DECIMAL(5,2)
-, selite 			VARCHAR(200)
-, PRIMARY KEY (tunti_id)
-, FOREIGN KEY (kayttaja_id) REFERENCES Kayttajat(kayttaja_id)
+CREATE TABLE Tunnit(
+ tunti_id 			SMALLINT NOT NULL AUTO_INCREMENT,
+ kayttaja_id 		SMALLINT NOT NULL,
+ pvm 				DATE NOT NULL, 
+ tuntien_maara	 	DECIMAL(5,2), 
+ selite 			VARCHAR(200), 
+PRIMARY KEY (tunti_id), 
+FOREIGN KEY (kayttaja_id) REFERENCES Kayttajat(id)
 )Engine=InnoDB DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE authority (
-  id integer NOT NULL auto_increment PRIMARY KEY,
+  id SMALLINT NOT NULL auto_increment PRIMARY KEY,
   role varchar(255) NOT NULL UNIQUE
  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Kayttaja_authority (
-  id integer NOT NULL auto_increment PRIMARY KEY,
-  kayttaja_id integer NOT NULL,
-  authority_id integer NOT NULL,
+  id SMALLINT NOT NULL auto_increment PRIMARY KEY,
+  kayttaja_id SMALLINT NOT NULL,
+  authority_id SMALLINT NOT NULL,
   FOREIGN KEY (kayttaja_id) REFERENCES Kayttajat(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
   FOREIGN KEY (authority_id) REFERENCES authority(id) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
